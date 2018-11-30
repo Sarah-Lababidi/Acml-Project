c394faa1282f92f9fd6c93d9bab307f619b5e1ad
Sarah-Lababidi <38692962+Sarah-Lababidi@users.noreply.github.com>
2018-11-30 21:30:52 +0200

    2nd commit

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var request = require('request');
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/jobs", {useNewUrlParser: true});
+var Job = require("./models/jobs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -11,20 +14,53 @@ app.get("/", function(req, res){
     res.render("index");
 });
 
-app.get("/jobs", function(req, res){
-    res.render("jobs");
-});
 
-app.get("/search", function(req, res){
-    var location = req.query.location;
-    var description = req.query.description;
+app.get("/jobs", function(req, res){
+    location = req.query.location;
+    description = req.query.description;
     var url = "https://jobs.github.com/positions.json?description="+description+"&location="+location;
-    request(url, function(error, response, body){
-        if(!error&&response.statusCode==200){
-            var parsedData = JSON.parse(body);
-            res.render("jobs", {data: parsedData});
+    // Check if there exists a Job with these location and descriptions values in my database
+    Job.find({"location": { $regex: location, $options: 'i'}, "description": { $regex: description, $options: 'i'} }, function(err, jobs){
+        if(err){
+            console.log(err);
+        } else {
+            // check if jobs is null (I don't have this search result in my databse)-> make api request, save to database
+            if(jobs === undefined || jobs.length == 0) {
+                // make api request
+                request(url, function(error, response, body){
+                    if(!error&&response.statusCode==200){
+                       var parsedData = JSON.parse(body);
+                    }
+                    // save to database
+                    //console.log(parsedData);
+                    parsedData.forEach(function(element){
+                        Job.create({
+                            title: element["title"], 
+                            type: element["type"],
+                            descriptionUrl: element["url"],
+                            location: element["location"],
+                            companyName: element["company"],
+                            companyUrl: element["company_url"],
+                            description: element["description"]
+                        }, function(err, newjob){
+                            if(err){
+                                console.log(err);
+                            } else {
+                                console.log(newjob);
+                            }
+                        });
+                    });
+                });
+            } 
         }
-    })
+    });
+    Job.find({"location": { $regex: location, $options: 'i'}, "description": { $regex: description, $options: 'i'} }, function(err, jobs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("jobs", {jobs:jobs});
+        }
+    });
 });
 
 app.get("*", function(req, res){
---
+++ models/jobs.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/jobs", { useNewUrlParser: true });
+
+var jobSchema = new mongoose.Schema({
+    title: String, 
+    type: String,
+    descriptionUrl: String,
+    location: String,
+    companyName: String,
+    companyUrl: String,
+    // I won't display it but i will use it to check if this job exists in my DB
+    description: String
+});
+
+module.exports = mongoose.model("Job", jobSchema);
